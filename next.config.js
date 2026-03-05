6dccbd34f107ccd3e5bb83b07fc245e8b00a515d
jarredswhitevan <129229855+jarredswhitevan@users.noreply.github.com>
2026-03-05 09:50:28 -0500

    Refactor next.config.js for export and settings
    Removed image remote patterns and changed export syntax.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [{ protocol: "https", hostname: "**" }]
-  }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
